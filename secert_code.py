eed4da46764d09b575ccf3ec0fcecff691588a1d
DevakXD <101389277+DevakProprogrammer@users.noreply.github.com>
2022-10-21 13:08:04 +0530

    Add files via upload
    it took me more than 1.5hrs to make,but finally i am satisfied

---
+++ secert_code.py
@@ -0,0 +1,42 @@
+import art
+from art import logo
+import words
+from words import alphabet
+
+print(logo)
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def again():
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+
+shift %= 26
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+ask_again = input("Do you want to use it again? y or n")
+if ask_again == 'y':
+    again()
+else:
+    print("see you later!")
